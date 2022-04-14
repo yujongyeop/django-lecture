0141b8223a8eca2f4f788aee87ba05e6a52a9c8a
yujongyeop <230233@naver.com>
2022-04-14 15:07:48 +0900

    미리보기와 요약문 추가하기

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
