0404c5f8271bc24031f74420024d8085b1474cd3
yujongyeop <230233@naver.com>
2022-04-28 14:37:26 +0900

    테스트 추가

--- blog/tests.py
+++ blog/tests.py
@@ -7,18 +7,18 @@ class TestView(TestCase):
     def setup(self):
         self.client = Client()
 
-    def text_post_list(self):
+    def test_post_list(self):
         # 1.1
-        response = self.client.get('blog/')
+        response = self.client.get('/blog/')
         # 1.2
         self.assertEqual(response.status_code, 200)
         # 1.3
-        soup = BeautifulSoup(response.content, 'html.parse')
+        soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
         # 1.4
         navbar = soup.nav
         # 1.5
-        self.assertin('Blog', navbar.text)
+        self.assertIn('Blog', navbar.text)
         self.assertIn('About Me', navbar.text)
 
         # 2.1
@@ -49,3 +49,30 @@ class TestView(TestCase):
         # 3.4
         self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
 
+    def test_post_detail(self):
+        # 1.1
+        post_001 = Post.objects.create(
+            title='첫 번째 포스트입니다.',
+            content ='Hello World. We are the world.',
+        )
+        # 1.2
+        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+
+        # 2
+        # 2.1
+        response = self.client.get(post_001.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # 2.2
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        # 2.3
+        self.assertIn(post_001.title, soup.title.text)
+
+        # 2.4
+        main_area = soup.find('div', id='main-area')
+        post_area = main_area.find('div', id='post-area')
+        self.assertIn(post_001.title, post_area.text)
